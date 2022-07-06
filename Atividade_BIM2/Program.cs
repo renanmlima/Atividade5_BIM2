6173da5b74ae9a0b78ec5a54c8ab5adcc1546dff
Renan Lima <renanmlimabrasil@hotmail.com>
2022-07-06 17:22:28 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade_BIM2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_BIM2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string j = "S";
+            do
+            {
+
+                Console.Clear();
+                for (int i = 1; i <= 100; i++)
+                {
+
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+
+                }
+                Console.WriteLine();
+
+                Console.Write("Usar novamente? (S/N): ");
+
+                j = Console.ReadLine();
+
+                if (j != "N" && j != "n" && j != "S" && j != "s")
+                {
+
+                    Console.WriteLine("ERRO!");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+
+                }
+
+
+            }
+            while (j == "S" || j == "s");
+
+            Console.WriteLine("OBRIGADO POR USAR :)");
+            Console.ReadKey();
+        }
+    }
+}
