817185de59396f47687385a1d2bdb47eb94a365f
Renan Lima <105513340+renanmlima@users.noreply.github.com>
2022-07-06 17:29:09 -0300

    Update Program.cs

--- Atividade_BIM2/Program.cs
+++ Atividade_BIM2/Program.cs
@@ -8,6 +8,16 @@ namespace Atividade_BIM2
 {
     internal class Program
     {
+            /* |====================================================| */
+            /* |* AUTOR: RENAN LIMA | RA: 6321300 ******************| */
+            /* |* DATA DE CRIAÇÃO: 06.JUL.2022 *********************| */
+            /* |* PROVA FECHAMENTO DE NOTA DO 2 BIMESTRE ***********| */
+            /* |* MATÉRIA: LÓGICA DE PROGRAMAÇÃO *******************| */
+            /* |* SEMESTRE: 1 | INSTITUIÇÃO: UNIFAAT ATIBAIA *******| */
+            /* |* CURSO: ANÁLISE E DESENVOLVIMENTO DE SISTEMAS *****| */
+            /* |====================================================| */
+
+        
         static void Main(string[] args)
         {
 
